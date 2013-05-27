328347aa904365b5f4d255ee3548e2c6e0d16914
Maximilian Gaß <mxey@mxey.net>
2013-05-27 16:23:15 +0200

    Initial commit

---
+++ app.rb
@@ -0,0 +1,32 @@
+require 'sinatra'
+require 'open-uri'
+require 'json'
+require 'icalendar'
+require 'date'
+
+get '/' do
+  uri = 'http://bl0rg.net/~andi/gpn13-fahrplan.json'
+
+  fahrplan = JSON.parse(open(uri).read)
+  cal = Icalendar::Calendar.new
+
+  fahrplan.each do |fpe|
+    ice = Icalendar::Event.new
+    begin
+      ice.dtstart = DateTime.strptime(fpe['start'], '%Y%m%d-%H%M')
+    rescue ArgumentError
+    end
+  
+    begin
+      ice.dtend = DateTime.strptime(fpe['end'], '%Y%m%d-%H%M')
+    rescue ArgumentError
+    end
+    ice.summary = fpe['title']
+    ice.description = fpe['desc']
+    ice.location = fpe['place']
+    cal.add_event ice
+  end
+
+  headers 'Content-Type' => 'text/calendar'
+  return cal.to_ical
+end
